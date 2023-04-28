6a351fd46fccd4ac3c37954f72d58d48ba536939
Rostislav Král <rostislav-kral@seznam.cz>
2023-04-28 17:41:57 +0200

    Ghost

---
+++ src/Ghost.h
@@ -0,0 +1,16 @@
+//
+// Created by rostik on 4/28/23.
+//
+
+#ifndef MYQTPROJECT_GHOST_H
+#define MYQTPROJECT_GHOST_H
+#include <vector>
+#include "Coordinates.h"
+class Ghost {
+
+
+public:
+    int x, y;
+    Ghost();
+};
+#endif //MYQTPROJECT_GHOST_H
