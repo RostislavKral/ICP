fcdaedfbc0dfc3c4015230be39b866206e287829
Lukas Jezek <vedeni@jktech.cz>
2023-04-30 19:05:57 +0200

    refactored game entities numbers

---
+++ src/Game.cpp
@@ -0,0 +1,3 @@
+//
+// Created by jeza on 30.4.23.
+//
---
+++ src/Game.h
@@ -0,0 +1,28 @@
+//
+// Created by jeza on 30.4.23.
+//
+
+#ifndef MYQTPROJECT_GAME_H
+#define MYQTPROJECT_GAME_H
+#define FOOD 0
+#define PATH  1
+#define WALL 2
+#define G_BLINKY 3
+#define FINISH 4
+#define KEY 5
+#define GAME_START  6
+#define G_PINKY 7
+#define G_INKY 8
+#define G_CLYDE 9
+
+enum RunMode {
+    NONE = 0,
+    NORMAL = 1,
+    LOG = 2,
+    REPLAY = 3,
+    ENDGAME = 4
+};
+
+
+
+#endif //MYQTPROJECT_GAME_H
